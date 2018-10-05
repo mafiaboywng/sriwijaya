a21cb67502822ddcf8e4b2e7cf38823b1aadd453
mafiaboy <mafiaboy.wng@gmail.com>
2018-10-05 09:13:32 +0700

    modif router tambah cinta

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,8 @@ module.exports.routes = {
   '/': {
     view: 'pages/homepage'
   },
-  'GET /indonesia' : 'IndonesiaController.findAllIndonesia'
+  'GET /indonesia' : 'IndonesiaController.findAllIndonesia',
+  'GET /cinta' : 'IndonesiaController.cinta'
 
 
 };
