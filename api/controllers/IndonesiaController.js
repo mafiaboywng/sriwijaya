bfbc6ba7e6dd5248f942cac8cb25ececfabf3b92
mafiaboy <mafiaboy.wng@gmail.com>
2018-10-05 09:09:41 +0700

    add cinta

--- api/controllers/IndonesiaController.js
+++ api/controllers/IndonesiaController.js
@@ -3,6 +3,9 @@ module.exports = {
     findAllIndonesia: async (req, res) => {
         let indonesia = await Indonesia.find({kabupaten:'KABUPATEN SUKOHARJO'});
         return res.ok(indonesia);
+    },
+    cinta: async (req, res) => {
+        return res.ok('ILOVE U');
     }
 
 };
